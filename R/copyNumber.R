b54126d8385472bb6ecf22ff2b1acae133c9eccc
tfkillian <theo.killian@gmail.com>
2019-04-02 09:53:29 +0200

    redone data, .R, .Rd files with correct data headers
    Former-commit-id: cbc6e8f8d88c8a78f6b045c9ef2b5f41e133cd35

--- R/copyNumber.R
+++ R/copyNumber.R
@@ -4,31 +4,31 @@
 #' cancer cell lines. This dataset originates from the `public_19Q1_gene_cn` 
 #' file and has been converted from wide to the long form dataset. This dataset 
 #' contains 23299 Genes, 1604 Cell Lines, 38 Primary Diseases ans 33 Lineages. 
-#' The first column `depmap_ID` is a foreign key relating to the cell lineage, 
-#' followed by `cell_Line` which contains the common CCLE name of the cancer 
+#' The first column `depmap_id` is a foreign key relating to the cell lineage, 
+#' followed by `cell_line` which contains the common CCLE name of the cancer 
 #' cell lines, `gene` which contains both the HUGO gene name of the knockdown 
-#' gene along with entrez ID#, followed by the column `gene_Name` which only 
-#' contains HUGO gene name and `entrez_ID` which contains only the entrez ID#. 
-#' The last column `logCopyNumber` contains the numerical dependency score 
+#' gene along with entrez ID#, followed by the column `gene_name` which only 
+#' contains HUGO gene name and `entrez_id` which contains only the entrez ID#. 
+#' The last column `log_copy_number` contains the numerical dependency score 
 #' values for each pair of genes and cell lines.
 #'
 #' @format A long format data frame with 37371596 rows and 6 variables. The 
-#' numeric column `logCopyNumber` contains the log-fold change in copy number 
+#' numeric column `log_copy_number` contains the log-fold change in copy number 
 #' for a known gene and for a given cell line. 
 #' 
 #' \describe{
-#'      \item{depmap_ID}{cell line foreign key (i.e. "ACH-00001")}
-#'      \item{cell_Line}{Name of cancer cell line (i.e. "184A1_BREAST")}
+#'      \item{depmap_id}{cell line foreign key (i.e. "ACH-00001")}
+#'      \item{cell_line}{Name of cancer cell line (i.e. "184A1_BREAST")}
 #'      \item{gene}{HUGO symbol (e.g. "SAP25") and Entrez ID# (e.g. 100316904)}
-#'      \item{gene_Name}{HUGO symbol (e.g. "SAP25")}
-#'      \item{entrez_ID}{Entrez ID# (e.g. 100316904}
-#'      \item{logCopyNumber}{numerical log fold change in copy number for a 
+#'      \item{gene_name}{HUGO symbol (e.g. "SAP25")}
+#'      \item{entrez_id}{Entrez ID# (e.g. 100316904}
+#'      \item{log_copy_number}{numerical log fold change in copy number for a 
 #'      given gene and cell line}
 #' }
 #' 
 #' @details This data represents the `public_19Q1_gene_cn` dataset taken from
 #' the public depmap portal. This dataset features the addition of a foreign key
-#' `depmapID`` found in the first column of this dataset, which was added from 
+#' `depmap_id` found in the first column of this dataset, which was added from 
 #' the `metadata` dataset. The dataset has been converted to the long format.
 #' 
 #' @docType data
