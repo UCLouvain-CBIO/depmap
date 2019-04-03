1b7b5ef94bba34731f5669e23867629ac3d553e4
tfkillian <theo.killian@gmail.com>
2019-04-03 16:03:47 +0200

    updated .R, .Rd, visualization vignette and DESCRIPTION

--- R/TPM.R
+++ R/TPM.R
@@ -1,32 +1,33 @@
 #' TPM
 #'
-#' TPM is the CCLE RNAseq gene expression data at scale (log2(TPM+1)). This data 
-#' is derived from the `CCLE_depMap_19Q1_TPM`. This dataset contains 57820 
-#' Genes, 1165 Cell Lines, 33 Primary Diseases, 32 Lineages. This data only 
-#' shows expression data for protein coding genes. The first column is the 
-#' `depmap_id` is a foreign key relating to the cell lineage followed by 
-#' `cell_line` which contains the common CCLE name of the cancer cell lines, 
-#' `gene` which contains both the HUGO gene name of the knockdown gene along 
-#' with ensembl ID#, followed by the column `gene_name` which only contains HUGO
-#' gene name and `ensembl_id` which contains only the ensembl ID#. The last 
-#' column `expression` contains the numerical scale (log2(TPM+1)) protein coding
-#' gene expression change.
+#' The `TPM` dataset contains the CCLE "Transcript Per Million" RNAseq gene 
+#' expression data for protein coding genes. This dataset includes data from 
+#' 57820 genes, 1165 cancer cell lines, 33 primary diseases, 32 lineages. The 
+#' columns of `TPM` are: `depmap_id`, a foreign key corresponding to the cancer 
+#' cell lineage,`cell_line` the common CCLE name of the cancer cell lines, 
+#' `gene` containing both the HUGO gene name of the knockdown gene along with 
+#' ensembl ID#, `gene_name` containing the HUGO gene name and `ensembl_id` 
+#' containing only the ensembl ID# and `expression` which contains the numerical
+#' protein coding gene expression change at scale (log2(TPM+1)).
 #'
-#' @format A tibble data frame with 67360300 rows (cell lines) and 6 variables:
+#' @format A long format tibble data frame with 67360300 rows and 6 columns:
 #' \describe{
-#'      \item{depmap_id}{cell line foreign key (i.e. "ACH-000956")}
+#'      \item{depmap_id}{Cell line foreign key (i.e. "ACH-000956")}
 #'      \item{cell_line}{Name of cancer cell line (i.e. "22RV1_PROSTATE")}
 #'      \item{gene}{HUGO symbol and Ensembl ID (e.g. TSPAN6 (ENSG00000000003))}
 #'      \item{gene_name}{HUGO symbol (e.g. "TSPAN6")}
 #'      \item{ensembl_id}{Ensembl ID (e.g. ENSG00000044574)}
-#'      \item{expression}{Log fold scale (log2(TPM+1)) protein expression}
+#'      \item{expression}{Log fold scale (log2(TPM+1)) protein expression change}
 #' }
 #' 
-#' @details This data represents the `CCLE_depMap_19Q1_TPM` dataset taken from
-#' the public depmap portal. This dataset features the addition of a foreign key
-#' `depmap_id`` found in the first column of this dataset, which was added from 
-#' the `metadata` dataset. The dataset has been converted to the long format.
-#'
+#' @details This data originates from the `CCLE_depMap_19Q1_TPM` file taken 
+#' from the 19Q1 [Broad Institute](https://depmap.org/portal/download/) cancer 
+#' depenedency study. This dataset features the addition of a foreign key 
+#' `depmap_id` found in the first column of this dataset, which was added from 
+#' the `metadata` dataset. This dataset has been converted to a long format 
+#' tibble. Variables names from the original dataset were converted to lower 
+#' case, put in snake case, and abbreviated where feasible.
+#' 
 #' @docType data
 #'
 #' @usage data(TPM)
