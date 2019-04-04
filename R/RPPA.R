fc08d9e39753d2486d2df3137bf3993a61f6ee21
tfkillian <theo.killian@gmail.com>
2019-04-04 17:05:09 +0200

    backup of most changes, including depmap_release.R

--- R/RPPA.R
+++ R/RPPA.R
@@ -1,32 +1,35 @@
 #' RPPA
 #'
-#' Celluar Models dataset: CCLE Reverse Phase Protein Array (RPPA) data which 
-#' originates from the `CCLE_RPPA_20180123`. This dataset has been converted 
-#' from wide fromat to the long form dataset and contains 214 Genes, 899 Cell 
-#' Lines, 28 Primary Diseases, 28 Lineages. The first column is the `depmap_id` 
-#' is a foreign key relating to the cell lineage, followed by `cell_line` which 
-#' contains the common CCLE name of the cancer cell lines, `gene` which contains
-#' the knockdown gene expression, `antibody` containing the name of knocked down 
-#' gene and `expression` containing numerical protein expression data.
-#' 
-#' @format A data frame with 899 rows (cell lines) and 216 variables (genes):
+#' The `RPPA` dataset contains Reverse Phase Protein Array (RPPA) Western Blot
+#' expression data. This dataset includes data from 214 genes, 899 cancer cell
+#' lines, 28 primary diseases, 28 lineages. The columns of `RPPA` are:
+#' `depmap_id`, a foreign key corresponding to the cancer cell lineage,
+#' `cell_line` which contains the common CCLE name of the cancer cell lines,
+#' `gene` which contains the knockdown gene expression, `antibody` containing
+#' the name of knocked down gene and `expression` containing numerical protein
+#' expression data.
+#'
+#' @format A long format tibble data frame with 192386 rows and 4 columns:
 #' \describe{
-#'     \item{depmap_id}{cell line foreign key (i.e. "ACH-000001")}
-#'     \item{cell_line}{Name of cancer cell line (i.e. "NIHOVCAR3_OVARY")}
+#'     \item{depmap_id}{cancer cell line foreign key (i.e. "ACH-000001")}
+#'     \item{cell_line}{CCLE name of cancer cell line (i.e. "NIHOVCAR3_OVARY")}
 #'     \item{antibody}{Name of antibody targeting protein (i.e. "14-3-3_beta")}
-#'     \item{expression}{protein expression}
+#'     \item{expression}{Observed expression via RPPA of protein coding genes}
 #' }
 #'
-#' @details This data represents the `CCLE_RPPA_20180123` dataset taken from
-#' the public depmap portal. This dataset features the addition of a foreign key
-#' `depmap_id`` found in the first column of this dataset, which was added from 
-#' the `metadata` dataset. The dataset has been converted to the long format.
-#' 
+#' @details This data represents the `CCLE_RPPA_20180123` file taken from the
+#' 19Q1 [Broad Institute](https://depmap.org/portal/download/) cancer
+#' depenedency study. This dataset features the addition of a foreign key
+#' `depmap_id` found in the first column of this dataset, which was added from
+#' the `metadata` dataset. This dataset has been converted to a long format
+#' tibble. Variables names from the original dataset were converted to lower
+#' case, put in snake case, and abbreviated where feasible.
+#'
 #' @docType data
 #'
 #' @usage data(RPPA)
-#' 
-#' @import tibble 
+#'
+#' @import dplyr
 #'
 #' @keywords datasets
 #'
