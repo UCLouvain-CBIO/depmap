431cd59125defeb7aff80f592a3cebfae2a1ef62
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-04-16 12:47:39 +0200

    message in onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
+##' importFrom utils read.csv
 .onLoad <- function(libname, pkgname) {
-    message("This is depmap release ", depmap_release())
     fl <- system.file("extdata", "metadata.csv", package=pkgname)
     titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
     createHubAccessors(pkgname, titles)
+}
+
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage(message("This is depmap release ", depmap_release()))
 }
