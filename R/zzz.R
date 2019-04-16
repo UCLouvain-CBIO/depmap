50a157070e8f72f3906186c81b716abbbcc37f2f
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-04-16 12:58:55 +0200

    add importfrom

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 ##' @importFrom utils read.csv
+##' @importFrom ExperimentHub createHubAccessors
 .onLoad <- function(libname, pkgname) {
-    fl <- system.file("extdata", "metadata.csv", package=pkgname)
+    fl <- system.file("extdata", "metadata.csv", package = pkgname)
     titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
     createHubAccessors(pkgname, titles)
 }
