5b7c51787c7f6b6f1d7f626a716eda0e4c64c428
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-04-16 13:31:57 +0200

    syntax guide

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 ##' @importFrom ExperimentHub createHubAccessors
 .onLoad <- function(libname, pkgname) {
     fl <- system.file("extdata", "metadata.csv", package = pkgname)
-    titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+    titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
     createHubAccessors(pkgname, titles)
 }
 
