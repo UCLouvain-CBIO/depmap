46fe28edcb0498d05bea211c0a66e1c71ddd3745
tfkillian <theo.killian@gmail.com>
2019-06-25 20:11:55 +0200

    zzz.R update for 19Q2 metadata

--- R/zzz.R
+++ R/zzz.R
@@ -1,29 +1,23 @@
-# ##' @importFrom utils read.csv
-# ##' @importFrom ExperimentHub createHubAccessors
-# library("tidyverse")
-# .onLoad <- function(libname, pkgname) {
-# #                     fls <- dir(system.file("extdata", package = pkgname),
-# #                     full.names = TRUE,
-# #                     pattern = "metadata")
-# #                     sapply(fls, function(fl) {
-# #                     titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-# #                     createHubAccessors(pkgname, titles)
-# # })
-# #     # fl <- system.file("extdata", "metadata.csv", package = pkgname)
-# #     # titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-# #     # createHubAccessors(pkgname, titles)
-# #     
-#     fl <- system.file("extdata", "metadata_19Q1.csv", package = pkgname)
-#     titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-#     createHubAccessors(pkgname, titles)
-# 
-#     fl <- system.file("extdata", "metadata_19Q2.csv", package = pkgname)
-#     titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-#     createHubAccessors(pkgname, titles)
-# }
-# 
-# .onAttach <- function(libname, pkgname) {
-#     packageStartupMessage(paste0("This is depmap release ", depmap_release()))
-# }
-
-
+##' @importFrom utils read.csv
+##' @importFrom ExperimentHub createHubAccessors
+.onLoad <- function(libname, pkgname) {
+                    # fls <- dir(system.file("extdata", package = pkgname),
+                    # full.names = TRUE,
+                    # pattern = "metadata")
+                    # sapply(fls, function(fl) {
+                    # titles <- read.table(fl, stringsAsFactors = FALSE)$Title
+                    # createHubAccessors(pkgname, titles)
+# })
+    ## get metadata upon loading for 19Q1 datasets
+    fl <- system.file("extdata", "metadata_19Q1.csv", package = pkgname)
+    titles <- utils::read.csv(fl, stringsAsFactors = FALSE)$Title
+    ExperimentHub::createHubAccessors(pkgname, titles)
+    ## get metadata upon loading for 19Q2 datasets
+    fl <- system.file("extdata", "metadata_19Q2.csv", package = pkgname)
+    titles <- utils::read.csv(fl, stringsAsFactors = FALSE)$Title
+    ExperimentHub::createHubAccessors(pkgname, titles)
+}
+ 
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage(paste0("This is depmap release ", depmap_release()))
+}
