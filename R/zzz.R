1f1f9467ae5b6c2d40214fa521a01d7b2c2a66b2
tfkillian <theo.killian@gmail.com>
2019-08-08 17:47:50 +0200

    zzz.R change

--- R/zzz.R
+++ R/zzz.R
@@ -16,6 +16,10 @@
     fl <- system.file("extdata", "metadata_19Q2.csv", package = pkgname)
     titles <- utils::read.csv(fl, stringsAsFactors = FALSE)$Title
     ExperimentHub::createHubAccessors(pkgname, titles)
+    ## get metadata upon loading for 19Q3 datasets
+    # fl <- system.file("extdata", "metadata_19Q3.csv", package = pkgname)
+    # titles <- utils::read.csv(fl, stringsAsFactors = FALSE)$Title
+    # ExperimentHub::createHubAccessors(pkgname, titles)
 }
  
 .onAttach <- function(libname, pkgname) {
