cba2c149d187a71d421faddfbb6c1ff2cb685961
tfkillian <theo.killian@gmail.com>
2019-12-06 15:11:25 +0100

    export NAMESPACE bug fix

--- R/loading_functions.R
+++ R/loading_functions.R
@@ -13,27 +13,34 @@ depmap_data_loading <- function(name) {
 depmap_rnai <- function() 
    depmap_data_loading("rnai")
 
+#' @export
 depmap_crispr <- function()
    depmap_data_loading("crispr")
 
+#' @export
 depmap_copyNumber <- function()
    depmap_data_loading("copyNumber")
 
+#' @export
 depmap_RPPA <- function()
    depmap_data_loading("RPPA")
 
+#' @export
 depmap_TPM <- function()
    depmap_data_loading("TPM")
 
+#' @export
 depmap_metadata <- function()
    depmap_data_loading("metadata")
 
+#' @export
 depmap_mutationCalls <- function()
    depmap_data_loading("copyNumber")
 
 # depmap_drug_sensitivity <- function()
 #    # depmap_drug_sensitivity("drug_sensitivity")
 
+#' @export
 depmap_drug_sensitivity <- function() {
       eh <- AnnotationHub::query(eh, c("depmap", "drug_sensitivity"))
       eh_name <- names(eh)[length(eh)]
