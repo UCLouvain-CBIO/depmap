dcd088ee8129ae687e485bc97e2f84e335b33d16
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-03-16 11:05:12 +0100

    check connectivity before creating hub accessors

--- R/zzz.R
+++ R/zzz.R
@@ -3,11 +3,15 @@
 .onLoad <- function(libname, pkgname) {
     fls <- dir(system.file("extdata", package = pkgname),
                full.names = TRUE, pattern = "metadata")
-    sapply(fls,
-           function(fl) {
-               titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
-               ExperimentHub::createHubAccessors(pkgname, titles)
-           })
+    if (curl::has_internet()) {
+        sapply(fls,
+               function(fl) {
+                   titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
+                   ExperimentHub::createHubAccessors(pkgname, titles)
+               })
+    } else {
+        warning("Not internet connectivity. Unable to create Hub accessors.")
+    }
 }
 
 .onAttach <- function(libname, pkgname) {
